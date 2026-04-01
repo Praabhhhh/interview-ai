c34e8efafb0275c175dfda54f18d5b76185d837c
Prabhjot singh <prabhjotsin008@gmail.com>
2026-04-01 23:48:57 +0530

    fix vercel routing

--- Backend/server.js
+++ Backend/server.js
@@ -6,7 +6,7 @@ const connectToDB = require("./src/config/database")
 
 connectToDB()
 
-const PORT = process.env.PORT || 10000
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`)
